b300227f5c668d0fd7f1898cf6e6f9f8532552c3
Pedro Gabriel Gonçalves <85896745+PedroGGoncalves@users.noreply.github.com>
2022-01-21 11:21:25 -0300

    Update Projeto3.c

--- Projeto3.c
+++ Projeto3.c
@@ -2,17 +2,51 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXKEYS 3
+#define MINKEYS (MAXKEYS-1)/2
+#define NIL (-1)
+#define NOKEY -1
+#define NO 0
+#define YES 1
+#define PAGESIZE sizeof(BTPAGE)
 
 struct estrutura
   	{
     	char cliente[3],codfilme[3],nome[50], filme[50], genero[50];
   	}film; 
+ //////////
+ typedef struct{
+        int keycount; // number of keys in page
+        int key[MAXKEYS]; // the actual keys
+        int child[MAXKEYS+1]; // ptrs to rrns of descendants
+}BTPAGE;
+
+int root; // rrn of root page
+FILE *btfd; // global file descriptor for "btree.dat"
+
+/* prototypes */
+int insert (int rrn, int key, int *promo_r_child, int *promo_key);
+int btopen();
+void btclose();
+int getroot();
+void putroot(int root);
+int create_tree();
+int getpage();
+void btread (int rrn, BTPAGE *page_ptr);
+void btwrite(int rrn, BTPAGE *page_ptr);
+int create_root(int key, int left, int right);
+void pageinit(BTPAGE *p_page);
+int search_node(int key, BTPAGE *p_page, int *pos);
+void ins_in_page(int key,int r_child, BTPAGE *p_page);
+void split(int key, int r_child, BTPAGE *p_oldpage, int *promo_key, int *promo_r_child, BTPAGE *p_newpage); 	
+  	
+ ///////////////////////////// 	
 char *identifica_campo(char *ts,int num)
 {
 	if(num==1)
 		return strtok(ts,"#");
 }	
-int pega_registro(FILE *p_out, char *p_reg) //utilizado para saber se o registro est· vazio ou n„o
+int pega_registro(FILE *p_out, char *p_reg) //utilizado para saber se o registro est√° vazio ou n√£o
 {
      unsigned char bytes;
      unsigned int a=0 ;
@@ -29,9 +63,23 @@ void insercao(FILE  *out,FILE *insere)
     int temp,temp_2=0,tam_reg,tam_arq,temp3,tamanho;
 	char reg[160];
 	
+	//////////
+	int promoted; // boolean: tells if a promotion from below
+    int root, // rrn of root page
+    promo_rrn; // rrn promoted from below
+    int promo_key, // key promoted from below
+    key; // next key to insert in tree
+
+    if (btopen()){
+       root = getroot();
+    }else{
+       root = create_tree();
+    }
+    //////////////
+	
 	fseek(insere,0,SEEK_END); //coloca o arquivo no fim
 	tam_arq=ftell(insere);   //pega tamnho do arquivo
-	fseek(insere,0,0);	     //coloca o arquivo no inÌcio
+	fseek(insere,0,0);	     //coloca o arquivo no in√≠cio
 	
 	tam_reg=pega_registro(out,reg);
 	if (tam_reg==0) //nada no arquivo
@@ -40,19 +88,27 @@ void insercao(FILE  *out,FILE *insere)
    		fread(&film,sizeof(film),1,insere); 
    		temp=(ftell(insere))/156; 
    		sprintf(reg,"##%s#%s#%s#%s#%s#%c",film.cliente,film.codfilme,film.nome,film.filme,film.genero,temp);
-   		temp_2=strlen(reg); //temp2 armazenar· o tamanho do registro reg que ser· usado para se locomover no arquivo saida
+   		temp_2=strlen(reg); //temp2 armazenar√° o tamanho do registro reg que ser√° usado para se locomover no arquivo saida
    		//conf temp_2
 		sprintf(reg,"%c#%s#%s#%s#%s#%s#%c",temp_2,film.cliente,film.codfilme,film.nome,film.filme,film.genero,temp);
-   		fwrite(reg, sizeof(char), strlen(reg), out); //registro È escrito no arquivo
-   	
+   		fwrite(reg, sizeof(char), strlen(reg), out); //registro √© escrito no arquivo
+   		
+   		int con1,con2;
+   		con1=atoi(film.cliente);
+   		con2=atoi(film.codfilme);
+   		
+   		//printf("%d %d",con1,con2);
+   		
+          root = create_root((con1*100+con2), NIL, NIL);
+   		  printf("\nChave %d inserida com sucesso\n",(con1*100+con2));
    		}
-	else //j· tem conte˙do
+	else //j√° tem conte√∫do
 	{	
 		fseek(out,0,SEEK_END); 
 		temp3=ftell(out);
-		fseek(out,ftell(out)-1,0); //arquivo na penultima posiÁ„o
-		fread(&temp_2,sizeof(int),1,out); //ser· lido a temp_2 para saber em que parte do arquivo est·
-		fseek(out,0,SEEK_END); //coloca o arquivo saida na ultima posiÁ„o para inserÁ„o
+		fseek(out,ftell(out)-1,0); //arquivo na penultima posi√ß√£o
+		fread(&temp_2,sizeof(int),1,out); //ser√° lido a temp_2 para saber em que parte do arquivo est√°
+		fseek(out,0,SEEK_END); //coloca o arquivo saida na ultima posi√ß√£o para inser√ß√£o
 		fseek(insere,(temp_2)*156,0); //locomove o arquivo insere com a temp_2 salva
 
 		if((((int)temp_2)*156)>=tam_arq)  //se a temp_2 for maior que o tamanho arquivo insere significa que chegou ao fim
@@ -61,18 +117,232 @@ void insercao(FILE  *out,FILE *insere)
 		}
 		
    		fread(&film,sizeof(film),1,insere); 
-   		temp=(ftell(insere)+2)/156; //temp que ser· utilizada para se mover no arquivo insere
+   		temp=(ftell(insere)+2)/156; //temp que ser√° utilizada para se mover no arquivo insere
    		//coloca dados no registro reg
    		sprintf(reg,"##%s#%s#%s#%s#%s#%c",film.cliente,film.codfilme,film.nome,film.filme,film.genero,temp);
-   		temp_2=strlen(reg); //temp2 armazenar· o tamanho do registro reg que ser· usado para se locomover no arquivo saida
+   		temp_2=strlen(reg); //temp2 armazenar√° o tamanho do registro reg que ser√° usado para se locomover no arquivo saida
    		sprintf(reg,"%c#%s#%s#%s#%s#%s#%c",temp_2,film.cliente,film.codfilme,film.nome,film.filme,film.genero,temp);
-   		fwrite(reg, sizeof(char), strlen(reg), out); //registro È escrito no arquivo
-   	
+   		fwrite(reg, sizeof(char), strlen(reg), out); //registro √© escrito no arquivo
+   		
+   		int con1,con2;
+   		con1=atoi(film.cliente);
+   		con2=atoi(film.codfilme);
+   		
+   		//printf("%d %d",con1,con2);
+   		promoted = insert(root, (con1*100+con2), &promo_rrn, &promo_key);
+          if (promoted)
+          root = create_root(promo_key, root, promo_rrn);
+   		
 	}
 
-	fclose(out);  fclose(insere);
+	fclose(out);  fclose(insere);btclose(); 
 }
+//////////////
+int insert (int rrn, int key, int *promo_r_child, int *promo_key){
+       BTPAGE page, // current page
+       newpage; // new page created if split occurs
+       int found, promoted; // boolean values
+       int pos,
+       p_b_rrn; // rrn promoted from below
+       int p_b_key; // key promoted from below
+       
+       if (rrn == NIL){
+          *promo_key = key;
+          *promo_r_child = NIL;
+          return(YES);
+       }
+       
+       btread(rrn, &page);
+       found = search_node ( key, &page, &pos);
+       
+       if (found){
+          printf ("\nChave %d duplicada\n", key);  return(0);
+       }
+       
+       promoted = insert(page.child[pos], key, &p_b_rrn, &p_b_key);
+ 
+       if (!promoted){
+          return(NO);
+       }
+ 
+       if(page.keycount < MAXKEYS){
+       		printf("\nChave %d inserida com sucesso\n",p_b_key);//mudar de lugar para n repetir
+             ins_in_page(p_b_key, p_b_rrn, &page);
+             btwrite(rrn, &page);
+             return(NO);
+       }else{
+             split(p_b_key, p_b_rrn, &page, promo_key, promo_r_child, &newpage);
+             printf("\nChave %d inserida com sucesso\n",p_b_key);//mudar de lugar para n repetir
+			 btwrite(rrn, &page);
+             btwrite(*promo_r_child, &newpage);
+             return(YES);
+       }
+}
+int btopen(){
+         btfd = fopen("btree.bin", "r+b");
+         return (btfd > 0);
+}
+void btclose(){
+          fclose(btfd);
+}
+int getroot(){
+      int root;
 
+      fseek(btfd, 0, 0);
+      
+      if (fread(&root, sizeof(int), 1, btfd) == 0){
+         printf("Error: Unable to get root. \007\n");
+         exit(1);
+      }      
+      return (root); 
+}
+void putroot(int root){
+              fseek(btfd, 0, 0);
+              fwrite(&root, sizeof(int), 1, btfd);
+}
+int create_tree(){
+      
+      btfd = fopen("btree.bin","w+b");
+      fclose (btfd);
+      btopen();
+      
+      return 0;
+}
+int getpage(){
+      long addr;
+      
+      fseek(btfd, 0, 2);
+      addr = ftell(btfd);
+      
+      if (addr == 0)
+        return 0;
+      else { 
+             addr = addr - sizeof(int);
+             return ((int) addr / PAGESIZE);
+           }
+}
+void btread (int rrn, BTPAGE *page_ptr){
+       long addr;
+       
+       addr = ((long)rrn * (long)PAGESIZE) + sizeof(int);
+       fseek(btfd, addr, 0);
+       fread(page_ptr, PAGESIZE, 1, btfd);
+}
+void btwrite(int rrn, BTPAGE *page_ptr){
+              long addr;
+              
+              addr = ((long)rrn * (long)PAGESIZE) + sizeof(int);
+              fseek(btfd, addr, 0);
+              fwrite(page_ptr, PAGESIZE, 1, btfd);
+}
+int create_root(int key, int left, int right){
+                 BTPAGE page;  int rrn;
+                 
+                 rrn = getpage();
+                 pageinit (&page);
+                 page.key[0] = key;
+                 page.child[0] = left;
+                 page.child[1] = right;
+                 page.keycount = 1;
+                 btwrite(rrn, &page);
+                 putroot(rrn);
+                 return(rrn);
+}
+void pageinit(BTPAGE *p_page){
+ int j;
+ 
+     for (j = 0; j < MAXKEYS; j++){
+         p_page->key[j] = NOKEY;
+         p_page->child[j] = NIL;
+     }
+     p_page->child[MAXKEYS] = NIL;
+}
+int search_node(int key, BTPAGE *p_page, int *pos){
+ int i;
+
+     for (i = 0; i < p_page->keycount && key > p_page->key[i]; i++);
+     *pos = i;
+     if (*pos < p_page->keycount && key == p_page->key[*pos]){
+        return(YES);
+     }else{
+        return(NO);
+     }
+}
+void ins_in_page(int key,int r_child, BTPAGE *p_page){
+ int j;
+ 
+     for(j = p_page-> keycount; key < p_page->key[j-1] && j > 0; j--){
+           p_page->key[j] = p_page->key[j-1];
+           p_page->child[j+1] = p_page->child[j];
+     }
+     p_page->keycount++;
+     p_page->key[j] = key;
+     p_page->child[j+1] = r_child;
+}
+void split(int key, int r_child, BTPAGE *p_oldpage, int *promo_key, int *promo_r_child, BTPAGE *p_newpage){
+ int j;
+ int mid;
+ int workkeys[MAXKEYS+1];  int workchil[MAXKEYS+2];
+	printf("\nDivisao de noh\n");
+      for (j = 0; j < MAXKEYS; j++){
+          workkeys[j] = p_oldpage->key[j];
+          workchil[j] = p_oldpage->child[j];
+          //printf("%d %d A",p_oldpage->key[j],p_oldpage->child[j]);
+      }
+      //printf("\n");
+      //workkeys[0], workkeys[1] *p_newpage
+      workchil[j] = p_oldpage->child[j];
+      
+      for (j = MAXKEYS; key < workkeys[j-1] && j > 0; j--){
+          workkeys[j] = workkeys[j-1];
+          workchil[j+1] = workchil[j];
+      }
+      workkeys[j] = key;
+      workchil[j+1] = r_child;
+
+      *promo_r_child = getpage();
+      pageinit(p_newpage);
+      //Alterar aki para funcionar para ordem par (chave=3)
+      /*for (j = 0; j <= MINKEYS; j++){ //att mudei isso e qse foi
+          p_oldpage->key[j] = workkeys[j];
+          //printf("B%d %d %d\n",workkeys[j],workkeys[j+1+MINKEYS],NOKEY);
+          p_oldpage->child[j] = workchil[j];
+          p_newpage->key[j] = workkeys[j+1+MINKEYS];
+          p_newpage->child[j] = workchil[j+1+MINKEYS];
+          p_oldpage->key[j+MINKEYS] = NOKEY;
+          p_oldpage->child[j+1+MINKEYS] = NIL;
+          //printf("B%d %d %d\n",j,j+MINKEYS,j+1+MINKEYS);
+      }*/
+       p_oldpage->key[0] = workkeys[0];
+        p_oldpage->child[0] = workchil[0];
+        p_newpage->key[0] = workkeys[2];
+        p_newpage->child[0] = workchil[2];
+        
+        p_oldpage->key[1] = NOKEY;
+        p_oldpage->key[2] = NOKEY;//adicioei isso att
+        p_oldpage->child[2] = NIL;
+        p_oldpage->child[1] = workchil[1];
+        //att qse la
+        //talvez fazer ideia (ver anots)
+        p_newpage->key[1] = workkeys[3];
+        p_newpage->child[1] = workchil[3];
+        p_newpage->child[2] = workchil[4]; //ultima mudan√ßa e aparentemente certyo
+      
+      
+      p_newpage->keycount = MAXKEYS - MINKEYS;
+      p_oldpage->keycount = MINKEYS;
+      *promo_key = workkeys[MINKEYS];//att *promo_key = workkeys[MINKEYS-1];
+      /*printf("%d %d %d %d\n",workkeys[0],workkeys[1],workkeys[2],workkeys[3]);
+      printf("%d %d %d %d %d\n",workchil[0],workchil[1],workchil[2],workchil[3],workchil[4]);
+	  printf("%d %d %d %d\n",p_oldpage->key[0],p_oldpage->key[1],p_oldpage->key[2],p_oldpage->key[3]);
+	  printf("%d %d %d %d\n",p_newpage->key[0],p_newpage->key[1],p_newpage->key[2],p_newpage->key[3]);
+	  printf("%d %d %d %d\n",p_oldpage->child[0],p_oldpage->child[1],p_oldpage->child[2],p_oldpage->child[3]);
+      printf("%d %d %d %d\n",p_newpage->child[0],p_newpage->child[1],p_newpage->child[2],p_newpage->child[3]);
+      */
+	  //
+      printf("Chave %d promovida",*promo_key);
+} 
+///////////////
 int main()
 {
 	int op=1;
